3796f601d42a43ce888c0ffdaced3fc0332ef4f7
Đorđe Sević <djordjesevic@gmail.com>
2021-01-10 21:40:02 +0100

    Update metadata extraction

--- server/src/main/java/rs/ijz/server/controller/ResenjeZahtevOdbijenController.java
+++ server/src/main/java/rs/ijz/server/controller/ResenjeZahtevOdbijenController.java
@@ -3,6 +3,7 @@ package rs.ijz.server.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,9 +12,12 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import rs.ijz.server.dto.ResponseMessage;
+import rs.ijz.server.fuseki.MetadataExtractor;
 import rs.ijz.server.model.resenje_zahtev_odbijen.ResenjeZahtevOdbijen;
+import rs.ijz.server.service.DomParserService;
 import rs.ijz.server.service.ResenjeZahtevOdbijenService;
 
 @CrossOrigin
@@ -23,6 +27,12 @@ public class ResenjeZahtevOdbijenController {
 	
     @Autowired
     private ResenjeZahtevOdbijenService resenjeZahtevOdbijenService;
+    
+    @Autowired
+    private DomParserService domParserService;
+    
+    @Autowired
+    private MetadataExtractor metadataExtractor;
 
     @GetMapping(value = "")
     private ResponseEntity<List<ResenjeZahtevOdbijen>> findAll() {
@@ -63,5 +73,11 @@ public class ResenjeZahtevOdbijenController {
             e.printStackTrace();
             return ResponseEntity.badRequest().body(new ResponseMessage("Neuspesno kreiranje."));
         }
+    }
+    
+    @PostMapping(value = "/extract-metadata")
+    public ResponseEntity<String> extractMetadata(@RequestParam("file") MultipartFile file) throws Exception {
+    	metadataExtractor.extract(domParserService.readMultipartXMLFile(file));
+    	return new ResponseEntity<>("Metadata extraction finished.", HttpStatus.OK);
     }
 }
