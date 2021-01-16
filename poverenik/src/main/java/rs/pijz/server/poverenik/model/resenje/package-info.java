de8f99ab9c2671588b800fe9b3db962af8dafad5
Vladimir Gajčin <djvlada03@gmail.com>
2021-01-16 12:37:51 +0100

    Missing import & namespace

--- poverenik/src/main/java/rs/pijz/server/poverenik/model/resenje/package-info.java
+++ poverenik/src/main/java/rs/pijz/server/poverenik/model/resenje/package-info.java
@@ -1,12 +1,12 @@
 @XmlSchema(
-        namespace = "http://www.pijz.rs/resenje-zalba-osnovana",
+        namespace = "http://www.pijz.rs/resenje",
         xmlns = {
-                @XmlNs(namespaceURI = "http://www.pijz.rs/resenje-zalba-osnovana", prefix = "rzo"),
+                @XmlNs(namespaceURI = "http://www.pijz.rs/resenje", prefix = "r"),
                 @XmlNs(namespaceURI = "http://www.pijz.rs/common", prefix = "cmn")
         },
         elementFormDefault = XmlNsForm.QUALIFIED
 )
-package rs.pijz.server.poverenik.model.resenje_zalba_osnovana;
+package rs.pijz.server.poverenik.model.resenje;
 
 import javax.xml.bind.annotation.XmlNs;
 import javax.xml.bind.annotation.XmlNsForm;
