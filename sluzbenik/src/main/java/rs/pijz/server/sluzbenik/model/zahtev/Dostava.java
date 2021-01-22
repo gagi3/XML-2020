823437d43b3ed5f5b02bca59918b64c1278e2028
Vladimir Gajčin <djvlada03@gmail.com>
2021-01-22 14:25:15 +0100

    Zahtev model

---
+++ sluzbenik/src/main/java/rs/pijz/server/sluzbenik/model/zahtev/Dostava.java
@@ -0,0 +1,57 @@
+
+package rs.pijz.server.sluzbenik.model.zahtev;
+
+import javax.xml.bind.annotation.XmlEnum;
+import javax.xml.bind.annotation.XmlEnumValue;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for Dostava.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * <p>
+ * <pre>
+ * &lt;simpleType name="Dostava">
+ *   &lt;restriction base="{http://www.w3.org/2001/XMLSchema}string">
+ *     &lt;enumeration value="posta"/>
+ *     &lt;enumeration value="email"/>
+ *     &lt;enumeration value="faks"/>
+ *     &lt;enumeration value="drugo"/>
+ *   &lt;/restriction>
+ * &lt;/simpleType>
+ * </pre>
+ * 
+ */
+@XmlType(name = "Dostava", namespace = "http://www.pijz.rs/zahtev")
+@XmlEnum
+public enum Dostava {
+
+    @XmlEnumValue("posta")
+    POSTA("posta"),
+    @XmlEnumValue("email")
+    EMAIL("email"),
+    @XmlEnumValue("faks")
+    FAKS("faks"),
+    @XmlEnumValue("drugo")
+    DRUGO("drugo");
+    private final String value;
+
+    Dostava(String v) {
+        value = v;
+    }
+
+    public String value() {
+        return value;
+    }
+
+    public static Dostava fromValue(String v) {
+        for (Dostava c: Dostava.values()) {
+            if (c.value.equals(v)) {
+                return c;
+            }
+        }
+        throw new IllegalArgumentException(v);
+    }
+
+}
---
+++ sluzbenik/src/main/java/rs/pijz/server/sluzbenik/model/zahtev/TipZahteva.java
@@ -0,0 +1,57 @@
+
+package rs.pijz.server.sluzbenik.model.zahtev;
+
+import javax.xml.bind.annotation.XmlEnum;
+import javax.xml.bind.annotation.XmlEnumValue;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for TipZahteva.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * <p>
+ * <pre>
+ * &lt;simpleType name="TipZahteva">
+ *   &lt;restriction base="{http://www.w3.org/2001/XMLSchema}string">
+ *     &lt;enumeration value="obavestenje"/>
+ *     &lt;enumeration value="uvid"/>
+ *     &lt;enumeration value="kopija"/>
+ *     &lt;enumeration value="dostava"/>
+ *   &lt;/restriction>
+ * &lt;/simpleType>
+ * </pre>
+ * 
+ */
+@XmlType(name = "TipZahteva", namespace = "http://www.pijz.rs/zahtev")
+@XmlEnum
+public enum TipZahteva {
+
+    @XmlEnumValue("obavestenje")
+    OBAVESTENJE("obavestenje"),
+    @XmlEnumValue("uvid")
+    UVID("uvid"),
+    @XmlEnumValue("kopija")
+    KOPIJA("kopija"),
+    @XmlEnumValue("dostava")
+    DOSTAVA("dostava");
+    private final String value;
+
+    TipZahteva(String v) {
+        value = v;
+    }
+
+    public String value() {
+        return value;
+    }
+
+    public static TipZahteva fromValue(String v) {
+        for (TipZahteva c: TipZahteva.values()) {
+            if (c.value.equals(v)) {
+                return c;
+            }
+        }
+        throw new IllegalArgumentException(v);
+    }
+
+}
