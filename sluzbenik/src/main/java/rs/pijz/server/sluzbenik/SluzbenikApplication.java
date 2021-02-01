e9a52fa5868fea0a6564ee19ca4e4b1e8780f852
Đorđe Sević <djordjesevic@gmail.com>
2021-02-01 01:41:17 +0100

    Initialize [poverenik - sluzbenik] communication

--- sluzbenik/src/main/java/rs/pijz/server/sluzbenik/SluzbenikApplication.java
+++ sluzbenik/src/main/java/rs/pijz/server/sluzbenik/SluzbenikApplication.java
@@ -1,13 +1,32 @@
 package rs.pijz.server.sluzbenik;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import rs.pijz.server.sluzbenik.soap.client.ObavestenjeClient;
+import rs.pijz.server.sluzbenik.soap.communication.obavestenje.GetObavestenjeResponse;
 
 @SpringBootApplication
 public class SluzbenikApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SluzbenikApplication.class, args);
+    }
+    
+    @Bean
+    CommandLineRunner lookupObavestenje(ObavestenjeClient obavestenjeClient) {
+      return args -> {
+        String broj = "OB-001";
+
+        if (args.length > 0) {
+          broj = args[0];
+        }
+        
+        GetObavestenjeResponse response = obavestenjeClient.getObavestenje(broj);
+        System.err.println(response.getObavestenje().getBroj());
+      };
     }
 
 }
