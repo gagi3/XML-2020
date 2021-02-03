df05b499d93e16b62448f6210ca583edad589f09
Vladimir Gajčin <djvlada03@gmail.com>
2021-02-03 20:35:54 +0100

    Missing model annotations

--- poverenik/src/main/java/rs/pijz/server/poverenik/model/resenje/Resenje.java
+++ poverenik/src/main/java/rs/pijz/server/poverenik/model/resenje/Resenje.java
@@ -5,13 +5,7 @@ import rs.pijz.server.poverenik.model.common.*;
 
 import java.util.ArrayList;
 import java.util.List;
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlAttribute;
-import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlID;
-import javax.xml.bind.annotation.XmlSchemaType;
-import javax.xml.bind.annotation.XmlType;
+import javax.xml.bind.annotation.*;
 import javax.xml.bind.annotation.adapters.CollapsedStringAdapter;
 import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
 import javax.xml.datatype.XMLGregorianCalendar;
@@ -73,6 +67,7 @@ import javax.xml.datatype.XMLGregorianCalendar;
  * 
  * 
  */
+@XmlRootElement(name = "Resenje", namespace = "http://www.pijz.rs/resenje")
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlType(name = "Resenje", namespace = "http://www.pijz.rs/resenje", propOrder = {
     "metadata",
